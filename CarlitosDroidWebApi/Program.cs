478dd06108966c193a345f18c4f28abd3961352d
CLVargas <cl.vargas@globant.com>
2024-12-11 10:22:46 -0500

    Remove provisional

--- CarlitosDroidWebApi/Program.cs
+++ CarlitosDroidWebApi/Program.cs
@@ -14,10 +14,10 @@ var dbStringConnection = builder.Configuration.GetConnectionString("DefaultConne
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddHealthChecks();
-builder.Services.AddDbContext<UserDbContext>(options =>
+/*builder.Services.AddDbContext<UserDbContext>(options =>
     options.UseSqlServer(dbStringConnection)
 );
-builder.Services.AddScoped<UserService>();
+builder.Services.AddScoped<UserService>();*/
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options =>
@@ -28,13 +28,13 @@ builder.Services.AddSwaggerGen(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment() || app.Environment.IsStaging())
-{
+//if (app.Environment.IsDevelopment() || app.Environment.IsStaging())
+//{
     app.UseSwagger();
     app.UseSwaggerUI(options =>
         options.SwaggerEndpoint("/swagger/v1/swagger.json", "CarlitosDroid Web Api Definition v1")
     );
-}
+//}
 
 
 //app.UseRouting();
